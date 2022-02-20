abbcd89bba6b7ca75c52a2de84f637fac40a40f8
Bernieson Sahagun <berniesonsahagun@gmail.com>
2022-02-20 21:28:25 +0800

    tailwindcss setup

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -15,5 +15,14 @@ export default function Login() {
     document.title = 'Login | Instagram';
   }, []);
 
-  return <p>I&apos;m the login page</p>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="Iphone with Profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>Form</p>
+      </div>
+    </div>
+  );
 }
