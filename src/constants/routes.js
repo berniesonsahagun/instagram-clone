2447c24fd282de49ecd1b27af4f109a09bb2872b
Bernieson Sahagun <berniesonsahagun@gmail.com>
2022-02-20 18:59:30 +0800

    initial commit

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const LOGIN = '/login';
+export const DASHBOARD = '/';
+export const SIGN_UP = '/sign-up';
+export const PROFILE = '/profile/:username';
+export const NOT_FOUND = '/not-found';
---
+++ src/pages/login.jsx
@@ -0,0 +1,19 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  useEffect(() => {
+    document.title = 'Login | Instagram';
+  }, []);
+
+  return <p>I&apos;m the login page</p>;
+}
