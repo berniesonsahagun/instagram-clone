0edb5048e2d88ffd93d2690a60fb1065b0d9c593
Bernieson Sahagun <berniesonsahagun@gmail.com>
2022-03-07 19:58:10 +0800

    fixed bug with duplicate username checking

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import * as ROUTES from './constants/routes';
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 
-const login = lazy(() => import('./pages/login'));
+const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/sign-up"));
 
 function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <Route path={ROUTES.LOGIN} component={login} />
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
         </Switch>
       </Suspense>
     </Router>
