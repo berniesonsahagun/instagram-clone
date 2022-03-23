dd9552931453eb261f06d7c2cbb9b1141484e7fc
Bernieson Sahagun <berniesonsahagun@gmail.com>
2022-03-23 20:12:08 +0800

    added suggestions on sidebar

--- src/components/sidebar/suggestions.jsx
+++ src/components/sidebar/suggestions.jsx
@@ -1,3 +1,53 @@
-export default function Suggestions() {
-  return <p>Suggestions</p>;
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import PropTypes from "prop-types";
+import { getSuggestedProfiles } from "../../services/firebase";
+import SuggestedProfile from "./suggested-profile";
+
+export default function Suggestions({ userId, following, loggedInUserDocId }) {
+  const [profiles, setProfiles] = useState(null);
+
+  //get suggested profiles (firebase service)
+  useEffect(() => {
+    async function suggestedProfiles() {
+      const response = await getSuggestedProfiles(userId, following);
+      setProfiles(response);
+    }
+
+    if (userId) {
+      suggestedProfiles();
+    }
+  }, [userId, following]);
+
+  //get suggested profiles
+  //call the async function within useEffect
+  //store it in state
+  //go ahead and render
+
+  return !profiles ? (
+    <Skeleton count={2} height={150} className="mt-5" />
+  ) : profiles.length > 0 ? (
+    <div className="rounded flex flex-col">
+      <div className="text-sm flex items-center align-items justify-between mb-2">
+        <p className="font-bold text-gray-base">Suggestions for you</p>
+      </div>
+      <div className="mt-4 grid gap-5">
+        {profiles.map((profile) => (
+          <SuggestedProfile
+            key={profile.docId}
+            profileDocId={profile.docId}
+            username={profile.username}
+            profileId={profile.userId}
+            userId={userId}
+            loggedInUserDocId={loggedInUserDocId}
+          />
+        ))}
+      </div>
+    </div>
+  ) : null;
 }
+
+Suggestions.propTypes = {
+  userId: PropTypes.string,
+  following: PropTypes.array,
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/no-named-as-default */
+import "./wdyr";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
