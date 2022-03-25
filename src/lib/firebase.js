48193be7bd421bb818390831794fca55c1cf00c5
Bernieson Sahagun <berniesonsahagun@gmail.com>
2022-03-25 22:41:12 +0800

    finished on timeline and adding comments on posts

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/no-named-as-default */
-import Firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAGgRlZMLU-dk98D5dr7BXnAIsjKckKeXM",
-  authDomain: "instagram-clone-4fe28.firebaseapp.com",
-  projectId: "instagram-clone-4fe28",
-  storageBucket: "instagram-clone-4fe28.appspot.com",
-  messagingSenderId: "245890783284",
-  appId: "1:245890783284:web:63f8319aac433d4be65261",
+  apiKey: 'AIzaSyAGgRlZMLU-dk98D5dr7BXnAIsjKckKeXM',
+  authDomain: 'instagram-clone-4fe28.firebaseapp.com',
+  projectId: 'instagram-clone-4fe28',
+  storageBucket: 'instagram-clone-4fe28.appspot.com',
+  messagingSenderId: '245890783284',
+  appId: '1:245890783284:web:63f8319aac433d4be65261',
 };
 
 const firebase = Firebase.initializeApp(config);
